a1d4961dfd0f4a8abc46ee6dc30d0049ab2c8199
Dama Michael Yohanna <DamaMichaelYohanna>
2022-10-25 12:11:46 +0100

    add buid_ui function

--- main.py
+++ main.py
@@ -10,14 +10,28 @@ window = tk.Tk()
 
 window.mainloop()
 
-SAVE_PATH = tk.filedialog.askopen()
+##SAVE_PATH = tk.filedialog.askopen()
 
 link="https://www.youtube.com/watch?v=S4E4yAktjug"
 class MainWindow(ttk.Frame):
     def __init__(self, parent):
         ttk.Frame.__init__(self, parent)
+        self.parent = parent
+        self.parent.title("Down Tube")
+        self.parent.resizable(0, 0)
+        self.buid_ui()
 
     def build_ui(self):
+        main_frame = tk.Frame(self.parent,bg='white')
+        main_frame.grid()
+        #--------------------------
+        head_frame = tk.Frame(main_frame, bg='white')
+        head_frame.grid()
+        #-------------------------
+        body_frame = tk.Frame(main_frame, bg='white')
+        body_frame.grid()
+        tk.Label(body_frame, text='Download All you want',
+                 font='matura 16 bold', fg='purple').grid()
         pass
 #
 # try:
